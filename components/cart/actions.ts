1fd346b5d77a7ee0da52470cd46bbf8bfc72c978
danielbyiringiro <daniel.byiringiro@ashesi.edu.gh>
2024-10-23 11:17:13 +0000

    base repo

---
+++ components/cart/actions.ts
@@ -0,0 +1,65 @@
+'use server';
+
+import { addToCart, createCheckoutUrl, removeFromCart, updateCart } from 'lib/wix';
+import { ProductVariant } from 'lib/wix/types';
+import { revalidatePath } from 'next/cache';
+import { headers } from 'next/headers';
+import { redirect } from 'next/navigation';
+
+export const addItem = async (
+  _prevState: unknown,
+  item: {
+    productId: string;
+    variant?: ProductVariant;
+  }
+) => {
+  if (!item.productId) {
+    return 'Missing product ID';
+  }
+
+  try {
+    await addToCart([{ productId: item.productId, variant: item.variant, quantity: 1 }]);
+    revalidatePath('/', 'layout');
+  } catch (e) {
+    return 'Error adding item to cart';
+  }
+};
+
+export const removeItem = async (_prevState: unknown, lineId: string): Promise<String | undefined> => {
+  try {
+    await removeFromCart([lineId]);
+    revalidatePath('/', 'layout');
+  } catch (e) {
+    return 'Error removing item from cart';
+  }
+};
+
+export const updateItemQuantity = async (_prevState: unknown, {
+  lineId,
+  variantId,
+  quantity
+}: {
+  lineId: string;
+  variantId: string;
+  quantity: number;
+}): Promise<String | undefined> => {
+  try {
+    await updateCart([
+      {
+        id: lineId,
+        merchandiseId: variantId,
+        quantity
+      }
+    ]);
+    revalidatePath('/', 'layout');
+  } catch (e) {
+    console.error(e);
+    return 'Error updating item quantity';
+  }
+};
+
+export async function redirectToCheckout() {
+  const checkoutUrl = await createCheckoutUrl(new URL(headers().get('referer')!).origin);
+
+  redirect(checkoutUrl);
+}
---
+++ components/welcome-toast.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { useEffect } from 'react';
+import { toast } from 'sonner';
+
+export function WelcomeToast() {
+  useEffect(() => {
+    // ignore if screen height is too small
+    if (window.innerHeight < 650) return;
+    if (!document.cookie.includes('welcome-toast=2')) {
+      toast('🛍️ Iteme Shop!', {
+        id: 'welcome-toast',
+        duration: Infinity,
+        onDismiss: () => {
+          document.cookie = 'welcome-toast=2; max-age=31536000; path=/';
+        },
+        description: (
+          <>
+            Welcome to Iteme Shop, the one stop shop for all buyers and sellers in Ashesi.
+          </>
+        )
+      });
+    }
+  }, []);
+
+  return null;
+}
